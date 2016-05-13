e011548fbdc7543161b0c0d2b624185b048d4deb
gregferg <gregferg@gmail.com>
2016-05-13 12:56:23 -0700

    first commit

---
+++ lib/02_searchable.rb
@@ -0,0 +1,28 @@
+require_relative 'db_connection'
+require_relative '01_sql_object'
+
+module Searchable
+  def where(params)
+    where_line = []
+
+    params.each do |key, value|
+      where_line << "#{key} = ?"
+    end
+
+    where_line = where_line.join(" AND ")
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(results)
+  end
+end
+
+class SQLObject
+  extend Searchable
+end
